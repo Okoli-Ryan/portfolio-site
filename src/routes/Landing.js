247186c8f7975507328da4e5a0bf62decb06ed81
okoli-ryan50 <okoliryan50@gmail.com>
2020-11-13 08:46:13 +0100

    init

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Button from '../components/Button';
 import '../App.css';
-import Card from '../components/Card'
 import Hero from '../components/Hero';
 
 export default function Landing() {
 	return (
 		<>
 			<Hero />
-			
 		</>
 	);
 }
